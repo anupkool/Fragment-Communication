f86862635c657f0f9bd6c546cb7751750cf24e89
Anup Kulkarni <anup.sk@rediffmail.com>
2018-06-18 11:25:24 -0400

    Added a create shortcut button to have shortcut of the app on main screen

--- app/src/main/java/com/example/android/fragmentcommunication/MainActivity.java
+++ app/src/main/java/com/example/android/fragmentcommunication/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.android.fragmentcommunication;
 
+import android.content.Intent;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.FrameLayout;
 
 public class MainActivity extends AppCompatActivity {
@@ -68,5 +70,20 @@ public class MainActivity extends AppCompatActivity {
             fragmentTransaction.commit();
         }
     }
+    public void createShortcut(View view) {
+
+        Intent shortcutIntent = new Intent(this, MainActivity.class);
+        shortcutIntent.setAction(Intent.ACTION_MAIN);
+        Intent intent = new Intent();
+        intent.putExtra(Intent.EXTRA_SHORTCUT_INTENT, shortcutIntent);
+        intent.putExtra(Intent.EXTRA_SHORTCUT_NAME, getString(R.string.app_name));
+        intent.putExtra(Intent.EXTRA_SHORTCUT_ICON_RESOURCE, Intent.ShortcutIconResource.fromContext(this, R.mipmap.ic_launcher));
+        intent.setAction("com.android.launcher.action.INSTALL_SHORTCUT");
+        sendBroadcast(intent);
+    }
+
+
+
+
 
 }
